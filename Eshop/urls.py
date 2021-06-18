c8afc6f7054e8c942b2423629d3082f0d2dc14d0
DennyJohnsonP <dennyjohnsondj4@gmail.com>
2021-06-18 17:51:51 +0530

    Potiama Module Integrated

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
     path("portfolio",include('Portfolio.urls')),
+    path("contactus",include('myapp.urls')),
+    path("aboutus",include('website.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
